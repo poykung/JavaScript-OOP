30b9f95975be42c25de8a536a3cf124eb42c56b3
poykung <Thaweekhun.21@gmail.com>
2023-06-08 22:46:55 +0700

    create class and object

---
+++ how to create class&object.js
@@ -0,0 +1,21 @@
+//how to create class
+
+/*
+คำนิยาม class (ตามด้วยชื่อของ class){ ชื่อ class จะขึ้นต้นด้วยตัวพิมพ์ใหญ่
+    property & method
+}
+*/
+
+//ex.
+class User{
+
+}
+
+
+//how to create object
+
+// const or let (ชื่อ object) = new (ชื่อ class) (); จะ java script จะใส่ (); หรือไม่ใส่ก็ได้
+//ex. const user  = new User();
+
+
+
